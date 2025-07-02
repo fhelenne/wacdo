ad6b2de70153e52cc374752ae269c567ee39a1e9
fhelenne <helenne.f@gmail.com>
2025-07-02 12:04:44 +0200

    * suppression commentaires * ajout manifest

--- back/src/Repository/JobTitleRepository.php
+++ back/src/Repository/JobTitleRepository.php
@@ -15,29 +15,4 @@ class JobTitleRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, JobTitle::class);
     }
-
-    //    /**
-    //     * @return JobTitle[] Returns an array of JobTitle objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('j')
-    //            ->andWhere('j.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('j.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?JobTitle
-    //    {
-    //        return $this->createQueryBuilder('j')
-    //            ->andWhere('j.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
